623d184f2535cc792dd4c4b969eff6c165dce7ca
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2018-05-03 22:18:05 -0700

    Include Monitoring and Convert PollingAgent to .NET 4.6.1

---
+++ Monitoring/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Monitoring.Default" %>
+
+<%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Monitoring Home</title>
+</head>
+<body>
+    <p>Welcome</p>
+    
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="nnn" runat="server"></asp:TextBox>
+
+            <asp:DropDownList ID="ddlURL" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlURL_SelectedIndexChanged" Width="171px">
+                <asp:ListItem Text="" Value="" />
+            </asp:DropDownList>
+
+        </div>
+        <asp:Chart ID="LatencyChart" Type="Line" runat="server" Width="1197px">
+            <series>
+                <asp:Series ChartType="Line" Name="LatencySeries">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="LatencyChartArea">
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
+
+        <asp:Chart ID="StatusChart" Type="Line" runat="server" Width="1197px">
+            <series>
+                <asp:Series ChartType="Line" Name="StatusSeries">
+                </asp:Series>
+            </series>
+            <chartareas>
+                <asp:ChartArea Name="StatusChartArea">
+                </asp:ChartArea>
+            </chartareas>
+        </asp:Chart>
+    </form>
+
+</body>
+</html>
---
+++ Monitoring/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Web.UI.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
+
+namespace Monitoring
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!(ddlURL.Items.Count == 1 && String.IsNullOrEmpty(ddlURL.Items[0].Text)))
+            {
+                return;
+            }
+
+            String connString = ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
+            using (var conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+
+                String queryStr = "SELECT Url FROM config";
+                using (var cmd = new MySqlCommand(queryStr, conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var url = reader.GetString(0);
+                            ddlURL.Items.Add(url);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void ddlURL_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Series s = LatencyChart.Series["LatencySeries"];
+
+            String connString = ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
+            String url = ddlURL.SelectedValue;
+            using (var conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+
+                String latencyQuery = $"SELECT TimestampUtc, LatencyInMsec FROM manusys.data WHERE Url = '{url}' && TimestampUtc > subtime((SELECT MAX(TimestampUtc) AS LatestTimestampUtc FROM manusys.data), '03:00:00.000');";
+                DrawChart(LatencyChart.Series["LatencySeries"], conn, latencyQuery);
+
+                String statusQuery = $"SELECT TimestampUtc, HttpStatusCode FROM manusys.data WHERE Url = '{url}' && TimestampUtc > subtime((SELECT MAX(TimestampUtc) AS LatestTimestampUtc FROM manusys.data), '03:00:00.000');";
+                DrawChart(StatusChart.Series["StatusSeries"], conn, statusQuery);
+            }
+        }
+
+        private void DrawChart(Series s, MySqlConnection conn, string queryStr)
+        {
+            using (var cmd = new MySqlCommand(queryStr, conn))
+            {
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        DataPoint p = new DataPoint();
+                        p.XValue = DateTime.Parse(reader.GetString(0)).Ticks / 10000000;
+                        p.YValues = new double[] { Int32.Parse(reader.GetString(1)) };
+                        s.Points.Add(p);
+                    }
+                }
+            }
+        }
+    }
+}
